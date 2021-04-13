7e9e73b36e36a7397144f7791797247d0f793448
webabrakadabra <hireshnik@gmail.com>
2021-04-13 15:41:52 +0300

    3 proj

---
+++ gen.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python3
+
+import random
+
+DIGITS  = '0123456789'
+LOWERCASE_LETTERS = 'abcdefghijklmnopqrstuvwxyz'
+UPPERCASE_LETTERS = 'ABCDEFGHIJKLMNOPQRSTUVWXYZ'
+PUNCTUATION = '!#$%&*+-=?@^_'
+
+chars = ''
