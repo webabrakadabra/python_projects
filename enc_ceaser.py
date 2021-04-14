13f4fdbf09e817fb3876ed8920453427aa8e129c
webabrakadabra <hirehsnik@gmail.com>
2021-04-14 20:07:42 +0300

    'ceaser'

--- enc_ceaser.py
+++ enc_ceaser.py
@@ -40,9 +40,41 @@ def encrypt(lang, key, string):
 
     
 def decrypt(lang, key, string):
-    pass
+    newstring = ''
+    if lang == 'R':
+        for i in range(len(string)):
+            x = ord(string[i])
+            if 1040 <= x <= 1071:
+                x = ((x - 1040) - key) % 32
+                newstring += chr(x + 1040)
+                #print(x)
+            elif 1072 <= x <= 1103:
+                x = ((x - 1072) - key) % 32
+                newstring += chr(x + 1072)
+                #print(newstring)
+            else:
+                newstring += chr(x)
+        print(newstring)
+            
+    elif lang == 'E':
+        for i in range(len(string)):
+            x = ord(string[i])
+            if 65 <= x <= 90:
+                x = ((x - 65) - key) % 26
+                newstring += chr(x + 65)
+                #print(x)
+            elif 97 <= x <= 122:
+                x = ((x - 97) - key) % 26
+                newstring += chr(x + 97)
+                #print(newstring)
+            else:
+                newstring += chr(x)
+        print(newstring)
+    else:
+        print('Укажите язык алфавита!!! R - русский, E - английский')
     
     
 
 
 encrypt('E', 17, 'To be, or not to be, that is the question!')
+decrypt('E', 22, 'Hawnj pk swhg xabkna ukq nqj.')
